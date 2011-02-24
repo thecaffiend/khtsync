2a53db6217410a30d495f4632015a92faff647ae
Benoit HERVIER (Khertan) <khertan@khertan.net>
2011-02-24 10:18:55 +0100

    Improved sync

---
+++ khtsync.py
@@ -0,0 +1,408 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 Benoît HERVIER
+# Licenced under GPLv3
+
+from __future__ import with_statement
+
+""" Sync two folder over ssh """
+
+#TODO
+#Implement unit test
+
+import os
+import glob
+import paramiko
+#import md5
+import StringIO
+import pickle
+import relpth
+import rsync
+import sys
+import shutil
+import new
+
+def _closed(self):
+    return self._closed
+
+class Sync():
+
+    def __init__(self, hostname='127.0.0.1', port=22,
+             username=None,
+             password=None,
+             private_key_path='',
+             local_dir=None, remote_dir=None):
+
+        self.hostname = hostname
+        self.username = username
+        self.port = port
+        self.password = password
+        self.private_key_path = private_key_path
+        self.local_dir = local_dir
+        self.remote_dir = remote_dir
+
+    def connect(self):
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.set_missing_host_key_policy(paramiko.WarningPolicy)
+        print '*** Connecting...'
+        self.client.connect(self.hostname,
+                            self.port,
+                            self.username,
+                            self.password)
+
+    def close(self):
+        self.client.close()
+
+    def run(self, command):
+        log_fp = StringIO.StringIO()
+        status = 0
+        try:
+            t = self.client.exec_command(command)
+        except paramiko.SSHException:
+            status = 1
+        log_fp.write(t[1].read())
+        log_fp.write(t[2].read())
+        t[0].close()
+        t[1].close()
+        t[2].close()
+        return (status, log_fp.getvalue())
+
+    def isdir(self, path):
+        status = self.run('[ -d %s ] || echo "FALSE"' % path)
+        if status[1].startswith('FALSE'):
+            return False
+        return True
+
+    def isfile(self, path):
+        status = self.run('[ -d %s ] || echo "FALSE"' % path)
+        if status[1].startswith('FALSE'):
+            return False
+        return True
+    def exists(self, path):
+        status = self.run('[ -a %s ] || echo "FALSE"' % path)
+        if status[1].startswith('FALSE'):
+            return False
+        return True
+
+    def list_all(self, curr_path, remote_files, remote_dirs):
+        dirlist = self.sftp.listdir(curr_path)
+        for curr_file in dirlist:
+            relpath = relpth.relpath(self.remote_dir,os.path.join(curr_path, curr_file))
+            if self.isdir(os.path.join(curr_path, curr_file)):
+                remote_dirs[relpath] = self.sftp.lstat(os.path.join(curr_path, curr_file)).st_mtime
+                self.list_all(os.path.join(curr_path,
+                        curr_file), remote_files,remote_dirs)
+            else:
+                remote_files[relpath] = \
+                self.sftp.lstat(os.path.join(curr_path, curr_file)).st_mtime
+
+    def patch_from_server(self,relpath):
+        topatch = open(os.path.join(self.local_dir,relpath), "rb")
+        hashes = rsync.blockchecksums(topatch)
+
+        newfile = self.sftp.file(os.path.join(self.remote_dir,relpath))
+        newfile.closed = new.instancemethod(_closed, newfile, paramiko.SFTPFile)
+        delta = rsync.rsyncdelta(newfile, hashes)
+
+        topatch.seek(0)
+        fh = open(os.path.join(self.local_dir,relpath), "wb")
+        rsync.patchstream(topatch, fh, delta)
+        topatch.close()
+        newfile.close()
+        fh.close()        
+    
+    def patch_to_server(self,relpath):
+        topatch = self.sftp.file(os.path.join(self.remote_dir,relpath), "rb")
+        hashes = rsync.blockchecksums(topatch)
+
+        newfile = open(os.path.join(self.local_dir,relpath))
+        delta = rsync.rsyncdelta(newfile, hashes)
+
+        topatch.seek(0)
+        fh = self.sftp.file(os.path.join(self.remote_dir,relpath), "wb")
+        rsync.patchstream(topatch, fh, delta)
+        
+
+    def buildUpdate(self):
+        update = {}
+        update['delete_local'] = []
+        update['delete_remote'] = []
+        update['update_local'] = []
+        update['update_remote'] = []
+                
+        #Load remote last synced file
+        print '*** Loading remote last synced dirs and files...'
+        old_remote_files = {}
+        old_remote_dirs = {}
+        if self.exists(os.path.join(self.remote_dir,'.khtsync')):
+            try:
+                with self.sftp.file(os.path.join(self.remote_dir,'.khtsync') ,'rb') as fh:
+                    old_remote_files, old_remote_dirs = pickle.load(fh)
+            except:
+                pass
+
+        #Load local last synced file
+        print '*** Loading local last synced dirs and files....'
+        old_local_files = {}
+        old_local_dirs = {}
+        if os.path.exists(os.path.join(self.local_dir,'.khtsync')):
+            try:
+                with open(os.path.join(self.local_dir,'.khtsync') ,'rb') as fh:
+                    old_local_files, old_local_dirs = pickle.load(fh)
+            except:
+                pass
+                
+        #List remote files and dirs
+        print '*** Listing remote dirs and files...'
+        remote_files = {}
+        remote_dirs = {}
+        self.list_all(self.remote_dir, remote_files, remote_dirs)
+        if '.khtsync' in remote_files:
+            del remote_files['.khtsync']
+    
+        #List local files ands dirs
+        print '*** Listing local dirs and files...'
+        local_files = {}
+        local_dirs = {}
+        for root, dirs, files in os.walk(self.local_dir):
+            for afile in files:
+                path = os.path.join(root, afile)
+                local_files[relpth.relpath(self.local_dir,path)] = os.path.getmtime(path)
+            for dir in dirs:
+                path = os.path.join(root, dir)
+                local_dirs[relpth.relpath(self.local_dir,path)] = os.path.getmtime(path)
+                
+        if '.khtsync' in local_files:
+            del local_files['.khtsync']
+
+        print '*** listing deleted files and dirs...'
+
+        #Deleted local files
+        alist = list(set(old_local_files) - set(local_files))
+        for relpath in alist:
+            if relpath in remote_files:
+                if old_local_dirs[relpath]>=remote_files[relpath]:
+                    update['delete_remote'].append(relpath)
+            if relpath in remote_dirs:
+                if old_local_dirs[relpath]>=remote_dirs[relpath]:
+                    update['delete_remote'].append(relpath)
+
+        #Deleted local dirs
+        alist = list(set(old_local_dirs) - set(local_dirs))
+        for relpath in alist:
+            if relpath in remote_files:
+                if old_local_files[relpath]>=remote_files[relpath]:
+                    update['delete_remote'].append(relpath)
+            if relpath in remote_dirs:
+                if old_local_dirs[relpath]>=remote_dirs[relpath]:
+                    update['delete_remote'].append(relpath)
+
+        #Deleted remote files
+        alist = list(set(old_remote_files) - set(remote_files))
+        for relpath in alist:
+            if relpath in local_files:
+                if old_remote_files[relpath]>=local_files[relpath]:
+                    update['delete_local'].append(relpath)
+            if relpath in local_dirs:
+                if old_remote_dirs[relpath]>=local_dirs[relpath]:
+                    update['delete_local'].append(relpath)
+
+        #Deleted remote dirs
+        alist = list(set(old_remote_dirs) - set(remote_dirs))
+        for relpath in alist:
+            if relpath in local_files:
+                if old_local_remote[relpath]>=local_files[relpath]:
+                    update['delete_local'].append(relpath)
+            if relpath in local_dirs:
+                if old_remote_dirs[relpath]>=local_dirs[relpath]:
+                    update['delete_local'].append(relpath)
+
+        local_files.update(local_dirs)
+        remote_files.update(remote_dirs)
+        #New Local dirs
+#        print '*** listing new local dirs...'
+#        update['update_remote'].extend(list((set(local_dirs) - set(remote_dirs))))
+
+        #New Local files
+        print '*** listing new local files...'
+        update['update_remote'].extend(list((set(local_files) - set(remote_files))))
+                    
+        #New Remote dirs
+#        print '*** listing new remote dirs...'
+#        update['update_local'].extend(list((set(remote_dirs) - set(local_dirs))))
+
+        #New Remote files
+        print '*** listing new remote files...'
+        update['update_local'].extend(list((set(remote_files) - set(local_files))))
+
+        #Check modified files
+        print '*** listing modified files...'
+        for relpath in set(remote_files).intersection(local_files):
+            if local_files[relpath] > remote_files[relpath]:
+                update['update_remote'].append(relpath)
+            elif local_files[relpath] < remote_files[relpath]:
+                update['update_local'].append(relpath)
+
+        #Sorting update
+        update['delete_local'].sort()
+        update['delete_remote'].sort()
+        update['delete_local'].reverse()
+        update['delete_remote'].reverse()
+        update['update_local'].sort()
+        update['update_remote'].sort()
+        return update
+                            
+    def sync(self):
+        self.sftp = self.client.open_sftp()
+        update = self.buildUpdate()
+
+        print update
+
+        print '*** Deleting remote files and dirs...'        
+        for relpath in update['delete_remote']:
+            if self.isdir(os.path.join(self.remote_dir,relpath)):
+                self.sftp.rmdir(os.path.join(self.remote_dir,relpath))
+            else:
+                self.sftp.remove(os.path.join(self.remote_dir,relpath))
+
+        print '*** Deleting local files and dirs...'        
+        for relpath in update['delete_local']:
+            if os.path.isdir(os.path.join(self.local_dir,relpath)):
+                os.rmdir(os.path.join(self.local_dir,relpath))
+            else:
+                os.remove(os.path.join(self.local_dir,relpath))
+                
+        print '*** Uploading local files and dirs...'        
+        for relpath in update['update_remote']:
+            if os.path.isdir(os.path.join(self.local_dir,relpath)):
+                if self.exists(os.path.join(self.remote_dir,relpath)): #Old as a file
+                    if not self.isdir(os.path.join(self.remote_dir,relpath)): #Old as a file
+                        self.sftp.remove(os.path.join(self.local_dir,relpath))
+                print 'Debug mkdir ',os.path.join(self.remote_dir,relpath)
+                self.sftp.mkdir(os.path.join(self.remote_dir,relpath))
+                utime=os.path.getmtime(os.path.join(self.local_dir,relpath))
+                self.sftp.utime(os.path.join(self.remote_dir,relpath),(utime,utime))
+            else:
+                if self.exists(os.path.join(self.remote_dir,relpath)):
+                    if self.isdir(os.path.join(self.remote_dir,relpath)):
+                        self.sftp.rmdir(os.path.join(self.remote_dir,relpath))
+                        self.sftp.put(os.path.join(self.local_dir,relpath),os.path.join(self.remote_dir,relpath))
+                    else:
+                        self.patch_to_server(relpath)
+                else:
+                    print 'Debug put ',os.path.join(self.local_dir,relpath),os.path.join(self.remote_dir,relpath)
+                    self.sftp.put(os.path.join(self.local_dir,relpath),os.path.join(self.remote_dir,relpath))
+                utime=os.path.getmtime(os.path.join(self.local_dir,relpath))
+                self.sftp.utime(os.path.join(self.remote_dir,relpath),(utime,utime))
+                                    
+        print '*** Downloading local files and dirs...'        
+        for relpath in update['update_local']:
+            if self.isdir(os.path.join(self.remote_dir,relpath)):
+                if os.path.isfile(os.path.join(self.local_dir,relpath)): #Old as a file
+                    os.remove(os.path.join(self.local_dir,relpath))
+                os.mkdir(os.path.join(self.local_dir,relpath))
+                utime=self.sftp.lstat(os.path.join(self.remote_dir, relpath)).st_mtime
+                os.utime(os.path.join(self.local_dir,relpath),(utime,utime))
+            else:
+                if os.path.exists(os.path.join(self.local_dir,relpath)):
+                    if os.path.isdir(os.path.join(self.local_dir,relpath)):
+                        os.rmdir(os.path.join(self.local_dir,relpath))
+                        self.sftp.get(os.path.join(self.remote_dir,relpath),os.path.join(self.local_dir,relpath))
+                    else:
+                        self.patch_from_server(relpath)
+                else:
+                    self.sftp.get(os.path.join(self.remote_dir,relpath),os.path.join(self.local_dir,relpath))
+                utime=self.sftp.lstat(os.path.join(self.remote_dir, relpath)).st_mtime
+                os.utime(os.path.join(self.local_dir,relpath),(utime,utime))
+
+
+                
+#        try:
+#
+#            #Check new local dirs
+#            print '*** Checking new local dirs...'
+#            new_local = list((set(local_dirs) - set(remote_dirs)))
+#            new_local.sort()
+#            for relpath in new_local:
+#                self.sftp.mkdir(os.path.join(self.remote_dir,relpath))
+#                self.sftp.utime(os.path.join(self.remote_dir,relpath),(local_dirs[relpath],local_dirs[relpath]))
+#            del new_local
+#
+#            #Check new remote dirs
+#            print '*** Checking new remote dirs...'
+#            new_remote = list((set(remote_dirs) - set(local_dirs)))
+#            new_remote.sort()
+#            for relpath in new_remote:
+#                print 'Creating local dir %s' % (relpath)
+#                os.makedirs(os.path.join(self.local_dir,relpath))
+#                os.utime(os.path.join(self.local_dir,relpath),(remote_dirs[relpath],remote_dirs[relpath]))
+#
+#
+#            #Check new remote files
+#            print '*** Checking new remote files...'
+#            for relpath in (set(remote_files) - set(local_files)):
+#                self.sftp.get(os.path.join(self.remote_dir,relpath),os.path.join(self.local_dir,relpath))
+#                os.utime(os.path.join(self.local_dir,relpath),(remote_files[relpath],remote_files[relpath]))
+#
+#            #Check new local files
+#            print '*** Checking new local files...'
+#            for relpath in (set(local_files) - set(remote_files)):
+#                print 'Uploading %s to %s' % (relpath,os.path.join(self.remote_dir,relpath))
+#                self.sftp.put(os.path.join(self.local_dir,relpath),os.path.join(self.remote_dir,relpath))
+#                self.sftp.utime(os.path.join(self.remote_dir,relpath),(local_files[relpath],local_files[relpath]))
+#
+            #Check modified files
+#            print '*** Checking modified files...'
+#            for relpath in set(remote_files).intersection(local_files):
+#                if local_files[relpath] > remote_files[relpath]:
+#                    print 'RSync upload %s' % (relpath)                    
+#                    self.patch_to_server(relpath)
+#                    self.sftp.utime(os.path.join(self.remote_dir,relpath),(local_files[relpath],local_files[relpath]))
+#    
+#                elif local_files[relpath] < remote_files[relpath]:
+#                    print 'Rsync download %s' % (relpath)
+#                    self.patch_from_server(relpath)
+#                    os.utime(os.path.join(self.local_dir,relpath),(remote_files[relpath],remote_files[relpath]))
+#    
+#                else:
+#                    print 'Same %s' % (relpath,)
+                
+        #Write remote last synced files
+        print '*** Writing remote last synced dirs and files...'
+        remote_files = {}
+        remote_dirs = {}
+        self.list_all(self.remote_dir, remote_files,remote_dirs)
+        if '.khtsync' in remote_files:
+            del remote_files['.khtsync']
+        fh = self.sftp.file(os.path.join(self.remote_dir,'.khtsync') ,'wb')
+        pickle.dump((remote_files,remote_dirs),fh)
+        fh.close()
+
+        #Write local last synced files
+        print '*** Writing local last synced dirs and files...'
+        local_files = {}
+        local_dirs = {}
+        for root, dirs, files in os.walk(self.local_dir):
+            for afile in files:
+                path = os.path.join(root, afile)
+                local_files[relpth.relpath(self.local_dir,path)] =  os.path.getmtime(path)
+            for dir in dirs:
+                path = os.path.join(root, dir)
+                local_dirs[relpth.relpath(self.local_dir,path)] = os.path.getmtime(path)
+        if '.khtsync' in local_files:
+            del local_files['.khtsync']
+        with open(os.path.join(self.local_dir,'.khtsync') ,'wb') as fh:
+            pickle.dump((local_files,local_dirs),fh)
+        
+#        except:
+#            raise
+
+if __name__ == '__main__':
+    if len(sys.argv)<7:
+        print "Usage : host port login password local_dir remote_dir"
+    else:
+        s = Sync(hostname=sys.argv[1], port=int(sys.argv[2]), username=sys.argv[3], password=sys.argv[4], local_dir=sys.argv[5], remote_dir=sys.argv[6])
+        s.connect()
+        s.sync()
+        s.close()
---
+++ test_khtsync.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 Benoît HERVIER
+# Licenced under GPLv3
+
+USER = ''
+PASSWORD = ''
+
+"""Unit test for khtsync.py"""
+
+import khtsync
+import unittest
+import os
+import shutil
+import hashlib
+import time
+import sys
+
+class TestSync(unittest.TestCase):
+
+    def testCreateFolder(self):
+        """ Check the upload and download of folder """
+        try:
+            shutil.rmtree('/tmp/origin')
+            shutil.rmtree('/tmp/dest')
+        except:
+            pass
+        os.mkdir('/tmp/origin')
+        os.mkdir('/tmp/dest')
+        os.mkdir('/tmp/origin/test_origin_dir')
+        os.mkdir('/tmp/dest/test_dest_dir')
+        s = khtsync.Sync('127.0.0.1',22,USER,PASSWORD,local_dir = '/tmp/origin',remote_dir = '/tmp/dest')
+        s.connect()
+        s.sync()
+        s.close()
+        assert os.path.isdir('/tmp/origin/test_dest_dir') , 'Syncing folder from ssh didn work'
+        assert os.path.isdir('/tmp/dest/test_origin_dir') , 'Syncing folder to ssh didn work'
+
+    def testCreateFile(self):
+        """ Check the upload and download of files """
+        try:
+            shutil.rmtree('/tmp/origin')
+            shutil.rmtree('/tmp/dest')
+        except:
+            pass
+        os.mkdir('/tmp/origin')
+        os.mkdir('/tmp/dest')
+        fh = open('/tmp/dest/test_origin','w')
+        fh.write('test_origin')
+        fh.close()
+        fh = open('/tmp/origin/test_dest','w')
+        fh.write('test_dest')
+        fh.close()
+        s = khtsync.Sync('127.0.0.1',22,USER,PASSWORD,local_dir='/tmp/origin',remote_dir='/tmp/dest')
+        s.connect()
+        s.sync()
+        s.close()
+        assert os.path.isfile('/tmp/origin/test_dest') , 'Syncing file from ssh didn work'
+        assert os.path.isfile('/tmp/dest/test_origin') , 'Syncing file to ssh didn work'
+        fh = open('/tmp/dest/test_origin','r')
+        origin = fh.read()
+        fh.close()
+        fh = open('/tmp/origin/test_dest','r')
+        dest = fh.read()
+        fh.close()
+        assert hashlib.md5(origin) != hashlib.md5('test_origin') , 'Syncing file to ssh didn work diff in the md5'
+        assert hashlib.md5(dest) != hashlib.md5('test_dest') , 'Syncing file to ssh didn work diff in the md5'
+
+    def testFolderReplacedByFile(self):
+        try:
+            shutil.rmtree('/tmp/origin')
+            shutil.rmtree('/tmp/dest')
+        except:
+            pass
+        os.mkdir('/tmp/origin')
+        os.mkdir('/tmp/dest')
+        fh = open('/tmp/origin/test_origin','w')
+        fh.write('test_origin')
+        fh.close()
+        time.sleep(1)
+        os.mkdir('/tmp/dest/test_origin')
+        os.mkdir('/tmp/origin/test_dest')
+        time.sleep(1)
+        fh = open('/tmp/dest/test_dest','w')
+        fh.write('test_dest')
+        fh.close()
+        s = khtsync.Sync('127.0.0.1',22,USER,PASSWORD,local_dir='/tmp/origin',remote_dir='/tmp/dest')
+        s.connect()
+        s.sync()
+        s.close()
+        assert os.path.isfile('/tmp/origin/test_dest') , 'Syncing file from ssh didn work'
+        assert os.path.isdir('/tmp/dest/test_origin') , 'Syncing file to ssh didn work'
+        fh = open('/tmp/origin/test_dest','r')
+        dest = fh.read()
+        fh.close()
+        assert hashlib.md5(dest) != hashlib.md5('test_dest') , 'Syncing file to ssh didn work'
+        
+    def testRsyncedFile(self):
+        try:
+            shutil.rmtree('/tmp/origin')
+            shutil.rmtree('/tmp/dest')
+        except:
+            pass
+        os.mkdir('/tmp/origin')
+        os.mkdir('/tmp/dest')
+        fh = open('/tmp/dest/test_origin','w')
+        fh.write('test_origin1')
+        fh.close()
+        time.sleep(1)
+        fh = open('/tmp/origin/test_origin','w')
+        fh.write('test_origin')
+        fh.close()
+        fh = open('/tmp/origin/test_dest','w')
+        fh.write('test_dest1')
+        fh.close()
+        time.sleep(1)
+        fh = open('/tmp/dest/test_dest','w')
+        fh.write('test_origin')
+        fh.close()
+        s = khtsync.Sync('127.0.0.1',22,USER,PASSWORD,local_dir='/tmp/origin',remote_dir='/tmp/dest')
+        s.connect()
+        s.sync()
+        s.close()
+        fh = open('/tmp/origin/test_origin','r')
+        origin_test_origin = fh.read()
+        fh.close()
+        fh = open('/tmp/dest/test_origin','r')
+        dest_test_origin = fh.read()
+        fh.close()
+        fh = open('/tmp/origin/test_dest','r')
+        origin_test_dest = fh.read()
+        fh.close()
+        fh = open('/tmp/dest/test_dest','r')
+        dest_test_dest = fh.read()
+        fh.close()
+        assert hashlib.md5(origin_test_origin) != hashlib.md5(dest_test_origin) , 'Rsynced upload differ'
+        assert hashlib.md5(origin_test_dest) != hashlib.md5(dest_test_dest) , 'Rsynced download differ'
+                        
+if __name__ == "__main__":
+    unittest.main()   
